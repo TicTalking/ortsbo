e284fde6b81f00e696db802d8e82a5811518b88b
Chris Constantin <chrisc@tictalking.com>
2012-05-08 19:03:58 -0700

    added finder methods

--- lib/ortsbo/language.rb
+++ lib/ortsbo/language.rb
@@ -9,8 +9,31 @@ module Ortsbo
       @native_name = native_name
       @code = code
       @ietf_tag = ietf_tag
-    end 
+    end
+
+    def self.find_by_ietf_tag ietf_tag
+      LANGUAGES_BY_IETF_TAG[ietf_tag]
+    end
+
+    def self.find_by_code code
+      LANGUAGES_BY_CODE[code]
+    end
+
+    def self.code_by_ietf_tag ietf_tag
+      l = LANGUAGES_BY_IETF_TAG[ietf_tag]
+      l ? l.code : nil
+    end
+
+    def self.ietf_tag_by_code code
+      l = LANGUAGES_BY_CODE[code]
+      l ? l.ietf_tag : nil
+    end
   end
 
-  SUPPORTED_LANGUAGES = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'data', 'languages.yml')))
+  LANGUAGES = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'data', 'languages.yml')))
+
+  protected
+
+  LANGUAGES_BY_IETF_TAG = Hash[*LANGUAGES.map {|l| [l.ietf_tag, l]}.flatten]
+  LANGUAGES_BY_CODE = Hash[*LANGUAGES.map {|l| [l.code, l]}.flatten]
 end
--- spec/ortsbo_spec.rb
+++ spec/ortsbo_spec.rb
@@ -2,6 +2,22 @@ require 'ortsbo'
 
 describe Ortsbo do
   it 'returns all supported languages' do
-    Ortsbo::SUPPORTED_LANGUAGES.count.should eq 53
+    Ortsbo::LANGUAGES.count.should eq 53
+  end
+
+  it 'finds language by IETF tag' do
+    Ortsbo::Language.find_by_ietf_tag('zh-TW').english_name.should eq 'Chinese (Traditional)'
+  end
+
+  it 'finds language by code' do
+    Ortsbo::Language.find_by_code('ron').english_name.should eq 'Romanian'
+  end
+
+  it 'finds code by IETF tag' do
+    Ortsbo::Language.code_by_ietf_tag('zh-CN').should eq 'zhs'
+  end
+
+  it 'finds IETF tag by code' do
+    Ortsbo::Language.ietf_tag_by_code('ita').should eq 'it'
   end
 end
